b19966050f6e80625241e0b0cabc51177c441791
Seclud <82933148+Seclud@users.noreply.github.com>
2023-12-17 14:10:57 +0500

    Added visualization for predictions

--- Main_Model_Script.py
+++ Main_Model_Script.py
@@ -13,7 +13,9 @@ import open3d as o3d
 import tensorflow as tf
 import os
 from sklearn.model_selection import train_test_split
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from keras.models import load_model
 
 def create_pointnet_model(num_points):
     input_points = layers.Input(shape=(num_points, 3))
@@ -60,6 +62,23 @@ def extract_labels(point_cloud):
 
     return labels
 
+def visualize_predictions(model, point_clouds, labels):
+    # Select the first 8 point clouds and labels
+    point_clouds = point_clouds[:8]
+    labels = labels[:8]
+
+    # Use the model to make predictions
+    predictions = model.predict(point_clouds)
+    predictions = np.round(predictions).astype(int)
+
+    # Plot the point clouds with their predicted and actual labels
+    fig = plt.figure(figsize=(15, 10))
+    for i in range(8):
+        ax = fig.add_subplot(2, 4, i + 1, projection='3d')
+        ax.scatter(point_clouds[i, :, 0], point_clouds[i, :, 1], point_clouds[i, :, 2], c=predictions[i, :, 0], cmap='RdYlGn')
+        ax.set_title(f"True: {np.mean(labels[i]) > 0.5}, Pred: {np.mean(predictions[i]) > 0.5}")
+        ax.set_axis_off()
+    plt.show()
 
 def main():
     point_clouds = []
@@ -79,22 +98,25 @@ def main():
     # Split the data into training and validation sets
     point_clouds_train, point_clouds_val, labels_train, labels_val = train_test_split(point_clouds, labels,
                                                                                       test_size=0.2, random_state=42)
-    # Create the PointNet model
-    model = create_pointnet_model(num_points)
-    # Compile the model
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-    print(model.summary())
-    # Train the model
-    model.fit(point_clouds_train, labels_train, validation_data=(point_clouds_val, labels_val), epochs=10)
+    model_path = 'my_model.h5'
+    if os.path.exists(model_path):
+        model = load_model(model_path)
+    else:
+        model = create_pointnet_model(num_points)
+        # Compile the model
+        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        # Train the model
+        model.fit(point_clouds_train, labels_train, validation_data=(point_clouds_val, labels_val), epochs=10)
+        # Save the model
+        model.save(model_path)
 
     loss, accuracy = model.evaluate(point_clouds_train, labels_train)
 
-    # Save the model
-    model.save('my_model.keras')
 
     print(f"Test set accuracy: {accuracy * 100}%")
 
+    # Use the function to visualize the predictions
+    visualize_predictions(model, point_clouds_val, labels_val)
 
 
 if __name__ == '__main__':
