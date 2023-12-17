7ce33b2f17809bd07de1802093d7386961246486
Seclud <82933148+Seclud@users.noreply.github.com>
2023-12-17 14:00:40 +0500

    some test script for different things

--- test2.py
+++ test2.py
@@ -3,8 +3,16 @@ import numpy as np
 import open3d as o3d
 import os
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import plaidml
+ctx = plaidml.Context()
 
 
-cloud = o3d.io.read_point_cloud('Point clouds/0004.ply')
-o3d.visualization.draw_geometries([cloud])
-cloudPoints = np.asarray(cloud.points)
+# cloud = o3d.io.read_point_cloud('Point clouds/0004.ply')
+# o3d.visualization.draw_geometries([cloud])
+# cloudPoints = np.asarray(cloud.points)
+
+print(tf.config.list_physical_devices('GPU'))
+
+print(plaidml.devices(ctx)[0])
+
