5a55f84f415f962a2f52d89f1292f844e4f3b989
Seclud <82933148+Seclud@users.noreply.github.com>
2023-12-28 00:41:56 +0500

    Added correct labeling and visualization of current labeled models

--- Main_Model_Script.py
+++ Main_Model_Script.py
@@ -6,7 +6,7 @@
 #
 # plaidml.keras.install_backend()
 # unfortunately paidml is old and i need to downgrade everything to make it work, so going to use cpu
-
+import pandas as pd
 from keras import layers, models
 import numpy as np
 import open3d as o3d
@@ -50,26 +50,48 @@ def create_pointnet_model(num_points):
     return model
 
 
-def extract_labels(point_cloud):
+def extract_labels(point_cloud,filename):
     green = [0, 1, 0]
     red = [1, 0, 0]
 
     colors = np.asarray(point_cloud.colors)
-    labels = np.zeros((len(colors), 1))
+    labels_inside = np.zeros((len(colors), 1))
+    labels_outside = np.zeros((len(colors), 1))
 
-    labels[np.all(colors == green, axis=1)] = 1
-    labels[np.all(colors == red, axis=1)] = 0
+    print(colors)
+    labels_inside[np.all(colors == green, axis=1)] = 1
+    labels_outside[np.all(colors == red, axis=1)] = 1
 
-    return labels
+    labels_inside1 = [str(x[0]) + '\n' for x in labels_inside]
+    labels_outside1= [str(x[0]) + '\n' for x in labels_outside]
+    file_name_without_extension, file_extension = os.path.splitext(filename)
+    completeName_inside = os.path.join("labels", file_name_without_extension)
+    completeName_outside = os.path.join("labels", file_name_without_extension)
+    if not os.path.exists(completeName_inside):
+        os.makedirs(completeName_inside)
+    if not os.path.exists(completeName_outside):
+        os.makedirs(completeName_outside)
+    with open(os.path.join(completeName_inside, f'labels_inside{file_name_without_extension}.txt'), 'w') as f:
+        f.writelines(labels_inside1)
+    with open(os.path.join(completeName_outside, f'labels_outside{file_name_without_extension}.txt'), 'w') as f:
+        f.writelines(labels_outside1)
+
+    print(type(labels_inside1[1]))
+    print(labels_inside.tolist())
+    print(labels_inside[1])
+    print(type(labels_inside[0][0]))
+    return labels_inside
 
 def visualize_predictions(model, point_clouds, labels):
     # Select the first 8 point clouds and labels
-    point_clouds = point_clouds[:8]
-    labels = labels[:8]
+    point_clouds = point_clouds[50:]
+    labels = labels[50:]
+    print(labels.tolist())
 
     # Use the model to make predictions
     predictions = model.predict(point_clouds)
     predictions = np.round(predictions).astype(int)
+    print(predictions)
 
     # Plot the point clouds with their predicted and actual labels
     fig = plt.figure(figsize=(15, 10))
@@ -79,6 +101,13 @@ def visualize_predictions(model, point_clouds, labels):
         ax.set_title(f"True: {np.mean(labels[i]) > 0.5}, Pred: {np.mean(predictions[i]) > 0.5}")
         ax.set_axis_off()
     plt.show()
+
+    # Create new point clouds with the predicted colors
+    for i in range(8):
+        point_cloud = o3d.geometry.PointCloud()
+        point_cloud.points = o3d.utility.Vector3dVector(point_clouds[i])
+        point_cloud.colors = o3d.utility.Vector3dVector(predictions[i] * [0, 1, 0] + (1 - predictions[i]) * [1, 0, 0])
+        o3d.io.write_point_cloud(f'Predicted clouds/predicted_{i}.ply', point_cloud)
 
 def main():
     point_clouds = []
@@ -90,33 +119,33 @@ def main():
         point_cloud = o3d.io.read_point_cloud(filepath)
         points = np.asarray(point_cloud.points)
         point_clouds.append(points)
-        labels.append(extract_labels(point_cloud))
+        labels.append(extract_labels(point_cloud,filename))
     print('Read all files')
     point_clouds = np.array(point_clouds)
     labels = np.stack(labels, axis=0)
 
-    # Split the data into training and validation sets
-    point_clouds_train, point_clouds_val, labels_train, labels_val = train_test_split(point_clouds, labels,
-                                                                                      test_size=0.2, random_state=42)
-    model_path = 'my_model.h5'
-    if os.path.exists(model_path):
-        model = load_model(model_path)
-    else:
-        model = create_pointnet_model(num_points)
-        # Compile the model
-        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        # Train the model
-        model.fit(point_clouds_train, labels_train, validation_data=(point_clouds_val, labels_val), epochs=10)
-        # Save the model
-        model.save(model_path)
-
-    loss, accuracy = model.evaluate(point_clouds_train, labels_train)
-
-
-    print(f"Test set accuracy: {accuracy * 100}%")
-
-    # Use the function to visualize the predictions
-    visualize_predictions(model, point_clouds_val, labels_val)
+    # # Split the data into training and validation sets
+    # point_clouds_train, point_clouds_val, labels_train, labels_val = train_test_split(point_clouds, labels,
+    #                                                                                   test_size=0.2, random_state=42)
+    # model_path = 'my_model.h5'
+    # if os.path.exists(model_path):
+    #     model = load_model(model_path)
+    # else:
+    #     model = create_pointnet_model(num_points)
+    #     # Compile the model
+    #     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    #     # Train the model
+    #     model.fit(point_clouds_train, labels_train, validation_data=(point_clouds_val, labels_val), epochs=10)
+    #     # Save the model
+    #     model.save(model_path)
+    #
+    # loss, accuracy = model.evaluate(point_clouds_train, labels_train)
+    #
+    #
+    # print(f"Test set accuracy: {accuracy * 100}%")
+    #
+    # # Use the function to visualize the predictions
+    # visualize_predictions(model, point_clouds_val, labels_val)
 
 
 if __name__ == '__main__':
--- test2.py
+++ test2.py
@@ -5,14 +5,91 @@ import os
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import plaidml
-ctx = plaidml.Context()
+import matplotlib.pyplot as plt
+import pandas as pd
+import keras
 
-
-# cloud = o3d.io.read_point_cloud('Point clouds/0004.ply')
+filename='0197.ply'
+filename_without_ext, file_ext=os.path.splitext(filename)
+cloud = o3d.io.read_point_cloud(f'Point clouds/{filename}')
 # o3d.visualization.draw_geometries([cloud])
-# cloudPoints = np.asarray(cloud.points)
+cloudPoints = np.asarray(cloud.points)
+print(cloudPoints.shape)
 
-print(tf.config.list_physical_devices('GPU'))
+LABELS = ['inside', 'outside']
+COLORS = ['green', 'red']
+point_clouds, test_point_clouds = [], []
+point_cloud_labels, all_labels = [], []
 
-print(plaidml.devices(ctx)[0])
 
+def visualize_data(point_cloud, labels):
+    df = pd.DataFrame(
+        data={
+            "x": point_cloud[:, 0],
+            "y": point_cloud[:, 1],
+            "z": point_cloud[:, 2],
+            "label": labels,
+        }
+    )
+    fig = plt.figure(figsize=(15, 10))
+    ax = plt.axes(projection="3d")
+    for index, label in enumerate(LABELS):
+        c_df = df[df["label"] == label]
+        try:
+            ax.scatter(
+                c_df["x"], c_df["y"], c_df["z"], label=label, alpha=0.5, c=COLORS[index]
+            )
+        except IndexError:
+            pass
+    ax.legend()
+    plt.show()
+
+label_data, num_labels = {}, 0
+for label in LABELS:
+    label_file = os.path.join('labels', filename_without_ext,f'labels_{label}{filename_without_ext}.txt')
+    label_data[label] = np.loadtxt(label_file ).astype("float32")
+    num_labels = len(label_data[label])
+
+print(label_data)
+print(label_data['inside'].tolist())
+
+print(num_labels)
+
+try:
+    label_map = ["none"] * num_labels
+    for label in LABELS:
+        print(label)
+        for i, data in enumerate(label_data[label]):
+            if label == 'inside':
+                print(data, 'inside')
+            if label == 'outside':
+                print(data, 'outside')
+            label_map[i] = label if data == 1.0 else label_map[i]
+            if data == 1.0:
+                print(label_map[i])
+    print(label_map)
+    label_data = [
+        LABELS.index(label) if label != "none" else len(LABELS)
+        for label in label_map
+    ]
+    # Apply one-hot encoding to the dense label representation.
+    label_data = keras.utils.to_categorical(label_data, num_classes=len(LABELS) + 1)
+
+    point_clouds.append(cloud)
+    point_cloud_labels.append(label_data)
+    all_labels.append(label_map)
+except KeyError :
+    test_point_clouds.append(cloud)
+
+
+all_labels=all_labels[0]
+print(all_labels)
+print(cloudPoints[:, 0])
+print(cloudPoints[:, 1])
+print(cloudPoints[:, 2])
+print(len(all_labels))
+print(len(cloudPoints[:, 0]))
+print(len(cloudPoints[:, 1]))
+print(len(cloudPoints[:, 2]))
+
+visualize_data(cloudPoints, all_labels)
